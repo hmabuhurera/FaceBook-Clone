f0dcfb601b59d0c4d5153431084924721f439a39
hmabuhurera <160245362+hmabuhurera@users.noreply.github.com>
2024-11-24 12:53:33 +0500

    THis is next.js project Facebook Clone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,51 @@
-import Image from "next/image";
+ import Image from "next/image"
+ 
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+const Facebook = () => {
+  return(
+    <div 
+     className="
+        bg-gray-100
+        h-screen
+        flex
+        items-center
+        justify-center
+        ">
+        {/* First div */}
+        <div
+          className="text-3xl w-1/2 px-10">
+
+          <Image src="/picture/facebook.svg" width={300} height={100} alt="Facebook logo"></Image>
+         <p className="ml-8 -mt-3">
+          Facebook helps you connect and share with the people in your life.
+         </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+          {/* Second div */}
+
+          <div className="
+              bg-white flex flex-col p-2 rounded-xl w-1/4
+           "
+           >
+
+            <p className="text-center text-blue-600 ">Log in to Facebook</p>
+            <input className="my-2 border border-1 boder-gray-100 p-3 focus-outline outline-blue-600 rounded-md border-blue" type="email/number" placeholder="Email adress or phone number" />
+
+            <input className="my-2 border border-1 boder-gray-100 p-3 focus-outline outline-blue-600 rounded-md" type="password" placeholder="Password" />
+
+            <button className="bg-blue-600 text-white text-lg font-bol hover:bg-blue-700 p-2 rounded rounded-md">Log in</button>
+
+            <button className="cursor:pointer p-2 text-blue-600 text-sm text-center hover:underline">Forgotten Password?</button>
+            <span className="p-2">
+              <hr />
+            </span>
+            <button className="p-2 my-2 bg-green-500 text-md font-bold text-white rounded-md hover:bg-green-600 w-fit mx-auto">Create new account</button>
+
+        </div>
+
     </div>
-  );
+  )
 }
+
+export default Facebook
